561e075b800456885545edecd416b2e1091c5264
celine24 <peyrot.celine@gmail.com>
2015-08-07 22:14:04 +0200

    ajout bootstrap

---
+++ resources/views/back/template.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Ma Douce Parenthèse</title>
+    <link rel="stylesheet" href="{{ asset('css/back/bootstrap/bootstrap.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('css/back/bootstrap/bootstrap-theme.min.css') }}"/>
+
+</head>
+<body>
+
+<div id="container">
+
+@yield('content')
+
+</div>
+
+</body>
+</html>
